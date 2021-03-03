94c52be9a558f7cc4d7ace0360908dd5bc78cc35
Tizen Ye <yzs981130@126.com>
2021-03-03 17:55:45 +0800

    save last checkpoint in case of iteration too long

--- gcheckpoint/__init__.py
+++ gcheckpoint/__init__.py
@@ -1,18 +1,19 @@
 import signal
 import traceback
+import torch
 import torch.distributed
 
 
 class GracefulKiller:
     kill_now = False
     callback_function = None
-    args = None
 
-    def __init__(self, _callback, *args):
+    last_saved_checkpoint = None
+
+    def __init__(self, _callback):
         signal.signal(signal.SIGINT, self.exit_handler)
         signal.signal(signal.SIGTERM, self.exit_handler)
         self.callback_function = _callback
-        self.args = args
 
     def exit_handler(self, signum, frame):
         self.kill_now = True
@@ -21,8 +22,13 @@ class GracefulKiller:
             self.callback_function(*self.args)
         traceback.print_stack(frame)
         '''
-    def check_exit(self, *args):
+        if self.last_saved_checkpoint is not None:
+            print('saving last checkpoint')
+            torch.save(self.last_saved_checkpoint, 'last_checkpoint-autosave.pth.tar')
+
+    def check_exit(self, saved_checkpoint, *args):
+        self.last_saved_checkpoint = saved_checkpoint
         is_main = not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0
         if self.kill_now and is_main:
             print('kill now')
-            self.callback_function(*args)
+            self.callback_function(saved_checkpoint, *args)
